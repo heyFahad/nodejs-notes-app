11526bfea31f01cd79a548f8bb15055bd46ae1a6
Fahad J <Fahad_Javed008@yahoo.com>
2020-01-26 19:08:27 +0500

    Argument Parsing with yargs - Part 2

--- app.js
+++ app.js
@@ -5,12 +5,33 @@ const getNotes = require('./notes.js');
 // Customize yargs version
 yargs.version('1.1.0');
 
+//
+// Challenge: Add an option to yargs
+//
+// 1. Setup a body option for the add command
+// 2. Configure a description, make it required, and for it to be a string
+// 3. Log the body value in the handler function
+// 4. Test your work!
+
 // Create add command
 yargs.command({
   command: 'add',
   describe: 'Add a new note',
-  handler: function() {
-    console.log('Adding a new note!');
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string'
+    },
+    body: {
+      describe: 'Note body',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler: function(argv) {
+    console.log('Title: ' + argv.title);
+    console.log('Body: ' + argv.body);
   }
 });
 
@@ -48,4 +69,4 @@ yargs.command({
   }
 });
 
-console.log(yargs.argv);
+yargs.parse();
